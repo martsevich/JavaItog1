78d54c90ad638c2872656a75e66ed0a45decad03
Dzmitry Martsevich <dipups@tut.by>
2024-02-01 23:08:39 +0300

    1

---
+++ src/seminar1/Printer.java
@@ -0,0 +1,33 @@
+package seminar1;
+
+class Calculator {
+    public int calculate(char op, int a, int b) {
+        // Введите свое решение ниже
+
+
+    }
+}
+
+// Не удаляйте этот класс - он нужен для вывода результатов на экран и проверки
+public class Printer{
+    public static void main(String[] args) {
+        int a = 0;
+        char op = ' ';
+        int b = 0;
+
+        if (args.length == 0) {
+            // При отправке кода на Выполнение, вы можете варьировать эти параметры
+            a = 3;
+            op = '+';
+            b = 7;
+        } else {
+            a = Integer.parseInt(args[0]);
+            op = args[1].charAt(0);
+            b = Integer.parseInt(args[2]);
+        }
+
+        Calculator calculator = new Calculator();
+        int result = calculator.calculate(op, a, b);
+        System.out.println(result);
+    }
+}
